e45ce50adbf57960a3b539fdfc55eb5de5ea90d2
ninja-grass <moizr95@gmail.com>
2018-10-02 16:47:12 -0400

    hw08 done

--- HW08/shuffle.c
+++ HW08/shuffle.c
@@ -15,15 +15,43 @@ void divide(CardDeck orig_deck, CardDeck * upper_deck, CardDeck * lower_deck)
 		// For example: strncpy((upper_deck[i]).cards, orig_deck.cards,i+1);
 		// Update the size of the upper and lower decks after copying each element.
 			// For example: upper_deck[i].size = i+1;
+	int i; 
+	for (i = 0; i < orig_deck.size - 1; i++)
+	{
+		strncpy(upper_deck[i].cards, orig_deck.cards, i+1);
+		upper_deck[i].size = i+1;
+
+		strncpy(lower_deck[i].cards, orig_deck.cards+i+1, orig_deck.size-i-1);
+		lower_deck[i].size = orig_deck.size-i-1;
+	}
 
 		// Similarly perform the string copy, and size update operation on the lower deck.
 			//For example: strncpy((lower_deck[i]).cards, orig_deck.cards+i+1,orig_deck.size-i-1);
 		    // For example: lower_deck[i].size = orig_deck.size -i - 1;
-	}
 }
 #endif
 
 #ifdef TEST_INTER
+void interleaveHelper(CardDeck upper_deck, CardDeck lower_deck, CardDeck new_deck, int upperIndex, int lowerIndex, int index)
+{
+	if (index == new_deck.size)
+	{
+		print_deck(new_deck);
+		return;
+	}
+
+	if (upperIndex < upper_deck.size)
+	{
+		new_deck.cards[index] = upper_deck.cards[upperIndex];
+		interleaveHelper(upper_deck, lower_deck, new_deck, upperIndex + 1, lowerIndex, index + 1);
+	}
+	if (lowerIndex < lower_deck.size)
+	{
+		new_deck.cards[index] = lower_deck.cards[lowerIndex];
+		interleaveHelper(upper_deck, lower_deck, new_deck, upperIndex, lowerIndex + 1, index + 1);
+	}
+
+}
 void interleave(CardDeck upper_deck, CardDeck lower_deck)
 {
     // Follow instructions in the README, to understand the working of the recursive function.
@@ -33,6 +61,9 @@ void interleave(CardDeck upper_deck, CardDeck lower_deck)
     // Tip: To copy the elements of one array from one array to another (e.g., the array of cards in a CardDeck),
         //you could use memcpy(…).
         //The = operator will simply copy the address, not the elements themselves.
+	CardDeck new_deck;
+	new_deck.size = upper_deck.size + lower_deck.size;
+	interleaveHelper(upper_deck, lower_deck, new_deck, 0, 0, 0);
 }
 #endif
 
@@ -41,21 +72,35 @@ void shuffle(CardDeck orig_deck)
 {
     // declare a variable to hold the number of pairs
         // we can say that we have only size-1 possibility of pairs
+	int pairs = orig_deck.size - 1;
 
     // if number of pairs == 0; return;
+    if (pairs == 0) {return;}
 
 	// instantiate pointers to hold both upper and lower decks (after division)
 	   // For example: CardDeck * upper_deck = NULL;
+    CardDeck * upper_deck = NULL;
+    CardDeck * lower_deck = NULL; 
 
 	// allocate memory based on number of pairs
 	   //For example: upper_deck = malloc(numpairs*sizeof(CardDeck));
+    upper_deck = malloc(pairs * sizeof(CardDeck));
+    lower_deck = malloc(pairs * sizeof(CardDeck));
 
 	// call divideDeck to fill upper_deck and lower_deck
+	divide(orig_deck, upper_deck, lower_deck);
 
 	//run a loop through all the pairs
 		// for each pair of upper and lower deck call interleave()
 		// For example: interleave(upper_deck[i],lower_deck[i]);
+	int i;
+	for (i = 0; i < pairs; i++)
+	{
+		interleave(upper_deck[i], lower_deck[i]);
+	}
 
     // free memory allocated to upper and lower deck.
+    free(upper_deck);
+    free(lower_deck);
 }
 #endif
--- HW08/shuffle.h
+++ HW08/shuffle.h
@@ -6,6 +6,7 @@
 void divide(CardDeck orig_deck, CardDeck* upper_deck, CardDeck* lower_deck);
 
 void interleave(CardDeck upper_deck, CardDeck lower_deck);
+//void interleaveHelper(CardDeck upper_deck, CardDeck lower_deck, CardDeck new_deck, int upperIndex, int lowerIndex, int index);
 
 void shuffle (CardDeck orig_deck);
 
