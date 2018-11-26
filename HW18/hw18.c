158c426dc14c8014d0fde7c0e476c6f1d4fd8a16
ninja-grass <moizr95@gmail.com>
2018-11-26 18:08:20 -0500

    hw18

--- HW18/hw18.c
+++ HW18/hw18.c
@@ -67,7 +67,45 @@ void FindMin(ListNode* head)
 	the 3rd parameter (min2). 
 	Look at README, and expected output for more details.
 	*/
-
+	ListNode * sHead = head;
+	ListNode * iter = sHead->next;
+	ListNode * hold1 = sHead;
+	ListNode * hold2 = iter;
+	int minDist = FindDist(sHead->treenode, iter->treenode);
+	while(sHead != NULL)
+	{
+		iter = sHead->next;
+		while(iter != NULL)
+		{
+			int dist = FindDist(sHead->treenode, iter->treenode);
+			if(dist < minDist)
+			{
+				minDist = dist;
+				hold1 = sHead;
+				hold2 = iter;
+			}
+			iter = iter->next;
+		}
+		sHead = sHead->next;
+	}
+	int i = 0;
+	while(i < head->treenode->dimension)
+	{
+		if(hold1->treenode->data[i] < hold2->treenode->data[i])
+		{
+			sHead = hold1;
+			iter = hold2;
+			i = head->treenode->dimension;
+		}
+		else if (hold1->treenode->data[i] > hold2->treenode->data[i])
+		{
+			sHead = hold2;
+			iter = hold1;
+			i = head->treenode->dimension;
+		}
+		i++;
+	}
+	PrintAnswer(head, sHead, iter);
 }
 #endif
 
@@ -77,8 +115,15 @@ int FindDist(TreeNode* x, TreeNode* y)
 {
 	//find the eucledian distance between
 	// x->data and y->data
+	int dist = 0;
+	int i;
+	for(i = 0; i < x->dimension; i++)
+	{
+		dist += (x->data[i] - y->data[i]) * (x->data[i] - y->data[i]);
+	}
 	// DO NOT FIND SQUARE ROOT (we are working with int)
 	// return the distance
+	return dist;
 }
 #endif
 
@@ -87,11 +132,33 @@ int FindDist(TreeNode* x, TreeNode* y)
 ListNode* CreateNode(int n, int dim, int* arr)
 {
 	// check for malloc error
-
+	ListNode * ln = malloc(sizeof(ListNode));
+	if (ln == NULL)
+	{
+		fprintf(stderr, "List Node Allocation error\n");
+		return NULL;
+	}
+	ln->treenode = malloc(sizeof(TreeNode));
+	if (ln->treenode == NULL)
+	{
+		fprintf(stderr, "Tree Node Allocation error\n");
+		return NULL;
+	}
 	// initialize dim
+	ln->treenode->dimension = dim;
 	// both left and right childern will be NULL
+	ln->treenode->left = NULL;
+	ln->treenode->right = NULL;
 	// allocate memory for data
+	ln->treenode->data = malloc(sizeof(int) * dim);
+	int i;
+	for(i = 0; i < dim; i++)
+	{
+		ln->treenode->data[i] = arr[i];
+	}
 	// return a ListNode
+	ln->next = NULL;
+	return ln;
 }
 #endif
 
@@ -101,8 +168,24 @@ void LinkedListCreate(ListNode ** head, int n, int dim, FILE* fptr)
 {
 	// create temp node using CreateNode
 	// read from file into an array, pass array to CreateNode
+	int * arr = malloc(sizeof(int) * n * dim);
+	int i;
+	for (i = 0; i < n * dim; i++)
+	{
+		fscanf(fptr, "%d", &arr[i]);
+	}
 	// assign temp to that node
+	ListNode * ln = CreateNode(n, dim, arr);
+	ListNode * temp = ln;
 
 	// use a loop to create nodes for the remaining elements of the list.
+	for(i = dim; i < n * dim; i += dim)
+	{
+		ListNode * node = CreateNode(n, dim, &arr[i]);
+		temp->next = node;
+		temp = node;
+	}
+	*head = ln;
+	free(arr);
 }
 #endif
--- HW18/main.c
+++ HW18/main.c
@@ -10,20 +10,36 @@
 int main(int argc, char * * argv)
 {
 	// check for input error
-
+	if (argc < 2) 
+	{
+		fprintf(stderr, "Not enough input arguments\n");
+		return EXIT_FAILURE;
+	}
 	// open the file
-
 	// input file is in argv[1]
-
+	FILE * fptr = fopen(argv[1], "r");
 	// n is the element in the file
-
+	int n;
+	fscanf(fptr, "%d", &n);
 	// dim is the second element in the file
-
+	int dim;
+	fscanf(fptr, "%d", &dim);
 	// the rest of the data in the file are the datapoints.
-
 	// call LinkedListCreate
-
+	ListNode * head;
+	LinkedListCreate(&head, n, dim, fptr);
+	fclose(fptr);
 	// call FindMin
+	FindMin(head);
 
+	while (head != NULL)
+	{
+		ListNode * node = head->next;
+		free(head->treenode->data);
+		free(head->treenode);
+		free(head);
+		head = node;
+	}
+	return EXIT_SUCCESS;
 }
 #endif
