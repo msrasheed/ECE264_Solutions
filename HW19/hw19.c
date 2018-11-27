3d223b42089d25a3f14a357a275221ec6905ea1a
ninja-grass <moizr95@gmail.com>
2018-11-26 19:12:41 -0500

    hw19

--- HW19/hw19.c
+++ HW19/hw19.c
@@ -54,8 +54,18 @@ ListNode* FindCentroid(TreeNode* x, TreeNode* y)
 	Average x->treenode.data[1] and y->treenode.data[1] to new->treenode.data[1]
 	and so on
 	*/
-
+	int * arr = malloc(sizeof(int) * x->dimension);
+	int i;
+	for(i = 0; i < x->dimension; i++)
+	{
+		arr[i] = (x->data[i] + y->data[i]) / 2;
+	}
+	ListNode * ln = CreateNode(1, x->dimension, arr);
+	free(arr);
+	ln->treenode->left = x;
+	ln->treenode->right = y;
 	// Return the new node
+	return ln;
 }
 #endif
 
@@ -63,7 +73,17 @@ ListNode* FindCentroid(TreeNode* x, TreeNode* y)
 #ifdef TEST_DIST
 int FindDist(TreeNode* x, TreeNode* y)
 {
-	//Same as previous HW
+	//find the eucledian distance between
+	// x->data and y->data
+	int dist = 0;
+	int i;
+	for(i = 0; i < x->dimension; i++)
+	{
+		dist += (x->data[i] - y->data[i]) * (x->data[i] - y->data[i]);
+	}
+	// DO NOT FIND SQUARE ROOT (we are working with int)
+	// return the distance
+	return dist;
 }
 #endif
 
@@ -71,12 +91,29 @@ int FindDist(TreeNode* x, TreeNode* y)
 #ifdef TEST_FUSE
 ListNode* Fuse(ListNode* head, ListNode* fuse1, ListNode* fuse2)
 {
-
 	// Create a new ListNode element using findCentroid function.
+	ListNode * ln = FindCentroid(fuse1->treenode, fuse2->treenode);
 	// The new->treenode.data will hold the averaged values of the two parameters' data (fuse1, and fuse2)
 	// add the new ListNode to the list.
+	ln->next = head;
 	// remove the fuse1 and fuse2 from the list. (Do NOT free)
+	head = ln;
+	ListNode * p = head;
+	ListNode * n = p->next;
+	while(n != NULL)
+	{
+		if(n == fuse1 || n == fuse2)
+		{
+			p->next = n->next;
+		}
+		else
+		{
+			p = p->next;
+		}
+		n = p->next;
+	}
 	// you may want to return head from this function (depending on your implementation)
+	return head;
 }
 #endif
 
@@ -84,7 +121,34 @@ ListNode* Fuse(ListNode* head, ListNode* fuse1, ListNode* fuse2)
 #ifdef TEST_CREATENODE
 ListNode* CreateNode(int n, int dim, int* arr)
 {
-	// Same as previous HW
+	// check for malloc error
+	ListNode * ln = malloc(sizeof(ListNode));
+	if (ln == NULL)
+	{
+		fprintf(stderr, "List Node Allocation error\n");
+		return NULL;
+	}
+	ln->treenode = malloc(sizeof(TreeNode));
+	if (ln->treenode == NULL)
+	{
+		fprintf(stderr, "Tree Node Allocation error\n");
+		return NULL;
+	}
+	// initialize dim
+	ln->treenode->dimension = dim;
+	// both left and right childern will be NULL
+	ln->treenode->left = NULL;
+	ln->treenode->right = NULL;
+	// allocate memory for data
+	ln->treenode->data = malloc(sizeof(int) * dim);
+	int i;
+	for(i = 0; i < dim; i++)
+	{
+		ln->treenode->data[i] = arr[i];
+	}
+	// return a ListNode
+	ln->next = NULL;
+	return ln;
 }
 #endif
 
@@ -92,7 +156,27 @@ ListNode* CreateNode(int n, int dim, int* arr)
 #ifdef TEST_LINKEDLISTCREATE
 void LinkedListCreate(ListNode ** head, int n, int dim, FILE* fptr)
 {
-	// Same as previous HW
+	// create temp node using CreateNode
+	// read from file into an array, pass array to CreateNode
+	int * arr = malloc(sizeof(int) * n * dim);
+	int i;
+	for (i = 0; i < n * dim; i++)
+	{
+		fscanf(fptr, "%d", &arr[i]);
+	}
+	// assign temp to that node
+	ListNode * ln = CreateNode(n, dim, arr);
+	ListNode * temp = ln;
+
+	// use a loop to create nodes for the remaining elements of the list.
+	for(i = dim; i < n * dim; i += dim)
+	{
+		ListNode * node = CreateNode(n, dim, &arr[i]);
+		temp->next = node;
+		temp = node;
+	}
+	*head = ln;
+	free(arr);
 }
 #endif
 
@@ -104,6 +188,53 @@ void MakeCluster(ListNode** head)
 	// fuse the two nodes into one node.
 	// call print function
 	// repeat till one node is remaining.
+	ListNode * h = *head;
+	while(h->next != NULL)
+	{
+		ListNode * sHead = h;
+		ListNode * iter = sHead->next;
+		ListNode * hold1 = sHead;
+		ListNode * hold2 = iter;
+		int minDist = FindDist(sHead->treenode, iter->treenode);
+		while(sHead != NULL)
+		{
+			iter = sHead->next;
+			while(iter != NULL)
+			{
+				int dist = FindDist(sHead->treenode, iter->treenode);
+				if(dist < minDist)
+				{
+					minDist = dist;
+					hold1 = sHead;
+					hold2 = iter;
+				}
+				iter = iter->next;
+			}
+			sHead = sHead->next;
+		}
+		int i = 0;
+		while(i < h->treenode->dimension)
+		{
+			if(hold1->treenode->data[i] < hold2->treenode->data[i])
+			{
+				sHead = hold1;
+				iter = hold2;
+				i = h->treenode->dimension;
+			}
+			else if (hold1->treenode->data[i] > hold2->treenode->data[i])
+			{
+				sHead = hold2;
+				iter = hold1;
+				i = h->treenode->dimension;
+			}
+			i++;
+		}
+		h = Fuse(h, sHead, iter);
+		PrintAns(h, sHead, iter);
+		free(sHead);
+		free(iter);
+	}
+	*head = h;
 }
 #endif
 
--- HW19/main.c
+++ HW19/main.c
@@ -8,20 +8,48 @@
 
 
 #ifdef TEST_MAIN
+void FreeBinaryTree(TreeNode *root);
+
 int main(int argc, char * * argv)
 {
 	// check for input error
-
+	if (argc < 2) 
+	{
+		fprintf(stderr, "Not enough input arguments\n");
+		return EXIT_FAILURE;
+	}
 	// open the file
-
+	FILE * fptr = fopen(argv[1], "r");
 	// n is availavle in argv[1]
-
+	int n;
+	fscanf(fptr, "%d", &n);
 	// dim is available in argv[2]
-
+	int dim;
+	fscanf(fptr, "%d", &dim);
 	// input file is in argv[3]
 	
 	// Create linkedList
-	
+	ListNode * head;
+	LinkedListCreate(&head, n, dim, fptr);
+	fclose(fptr);
 	// call MakeCluster
+	MakeCluster(&head);
+
+	FreeBinaryTree(head->treenode);
+	free(head);
+
+	return EXIT_SUCCESS;
+}
+
+void FreeBinaryTree(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    FreeBinaryTree(root->left);
+    FreeBinaryTree(root->right);
+    free(root->data);
+    free(root);
 }
 #endif
